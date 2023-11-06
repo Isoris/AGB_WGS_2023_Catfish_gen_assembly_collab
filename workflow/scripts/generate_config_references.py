620b2d961112b55460873fd4259783587d6a1d8f
= <=>
2023-11-06 16:25:00 +0700

    edited generate_references.py

--- workflow/scripts/generate_config_references.py
+++ workflow/scripts/generate_config_references.py
@@ -24,20 +24,6 @@ ref_id_counter = get_max_sample_id('../config/config_samples.yaml')
 # Continue with reading the references.csv and populating the references dictionary
 references = {}
 
-with open('../config/metadata_references/references.csv', 'r') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        species = row['Species_long']
-        if species not in references:
-            references[species] = {
-                # ... (rest of the initialization remains unchanged)
-            }
-        
-        # Increment the counter to generate a unique ref_id for each entry
-        ref_id_counter += 1
-        ref_id = f"ID{ref_id_counter}"
-        references[species]['reference_ids'].append(ref_id)
-
 with open('../config/metadata_references/combined_references.csv', 'r') as f:
     reader = csv.DictReader(f)
     for row in reader:
@@ -72,7 +58,24 @@ with open('../config/metadata_references/combined_references.csv', 'r') as f:
         ref_id_counter += 1
         ref_id = f"ID{ref_id_counter}"
         references[species]['reference_ids'].append(ref_id)
-        # ... (rest of your code remains unchanged)
+        references[species]['species_long'].append(row['Species_long'])
+        references[species]['sexes'].append(row['Sex'])
+        references[species]['methods'].append(row['Method'])
+        references[species]['orientations'].append(row['Orientation'])
+        references[species]['descriptions'].append(row['Description'])
+        references[species]['assembly_accessions'].append(row['Assembly_Accession'])
+        references[species]['assembly_names'].append(row['Assembly_Name'])
+        references[species]['organism_names'].append(row['Organism_Name'])
+        for infraspecific_name in ['Breed', 'Strain', 'Cultivar', 'Ecotype', 'Isolate', 'Sex']:
+            if row.get(f'Organism_Infraspecific_Names_{infraspecific_name}'):
+                references[species]['organism_infraspecific_names'][infraspecific_name.lower()].append(
+                    row[f'Organism_Infraspecific_Names_{infraspecific_name}']
+                )
+        references[species]['annotation_names'].append(row['Annotation_Name'])
+        references[species]['assembly_stats_total_sequence_length'].append(row['Assembly_Stats_Total_Sequence_Length'])
+        references[species]['assembly_level'].append(row['Assembly_Level'])
+        references[species]['assembly_release_dates'].append(row['Assembly_Release_Date'])
+        references[species]['wgs_project_accessions'].append(row['WGS_project_accession'])
 
 # Output to YAML format
 with open('../config/config_references.yaml', 'w') as f:
